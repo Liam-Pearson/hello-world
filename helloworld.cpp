ca678a821cbb68fb99d115f2c61ce0d7e1d7d1f9
Liam <111994335+Liam-Pearson@users.noreply.github.com>
2025-06-06 10:09:09 -0400

    Removed a redundant if statement.

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,20 +5,18 @@
 
 int sort()
 {
-    std::vector<int> sortVec {121,12,4673,540,5,66,75467,8,9,33636,2,3663,74,23,336,2574,25};
+    std::vector<int> sortVec {15321,12,4673,540,5,66,75467,8,9,33636,2,3663,74,23,336,2574,2546145};
 
     int vecEls= sortVec.size(); // size of vector.
     int largest = sortVec[0]; // initalizes largest number to the first position of the vector.
     int sortCount = 0; // starting position to sort from.
 
     for(int i =0; i<vecEls; i++){ // applies sorting algorithm to entire vector.
-        for (int i = sortCount; i < vecEls; i++) { // sorts for the largest number from the first position to the last position of the vector.
-            if((i+1) < vecEls) { // stops from doing the sorting comparison from outside the vector length (for last position only really).
-                if(sortVec[i+1] - largest > 0) { // if greater than 0, largest current number has been found.
-                    largest=sortVec[i+1]; // assigns largest number
-                    sortVec[i+1] = sortVec[sortCount]; // takes the current first number position and moves it to the position of the largest number in the vector.
-                    sortVec[sortCount] = largest; // move the current largest number to the first position of the vector.
-                }
+        for (int i = sortCount; i < vecEls-1; i++) { // sorts for the largest number from the first position to the last position of the vector.
+            if(sortVec[i+1] - largest > 0) { // if greater than 0, largest current number has been found.
+                largest=sortVec[i+1]; // assigns largest number
+                sortVec[i+1] = sortVec[sortCount]; // takes the current first number position and moves it to the position of the largest number in the vector.
+                sortVec[sortCount] = largest; // move the current largest number to the first position of the vector.  
             }
         }
         sortCount++; // increments the starting position of the vector.
